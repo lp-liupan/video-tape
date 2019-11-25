711ff9d390de4832c45b5d7274328f017fe31c9c
liupan <liupan@ebiot.com>
2019-11-25 18:47:39 +0800

    截图方法实现

--- src/main.js
+++ src/main.js
@@ -1,10 +1,68 @@
 console.log('hello word')
 
 import RecordRTC from 'recordrtc'
-class tape {
+class Tape {
 	
 	constructor(videoEl){
 		this.videoEl = videoEl;
+	}
+
+	/**
+	 *截图方法
+	 * @param {*} callback
+	 * @param {*} type
+	 * @param {*} fileName
+	 * @returns
+	 * @memberof Tape
+	 */
+	screenshot(callback,type='file',fileName='截图.png'){
+
+		//参数验证
+		if(!callback){
+			throw "截图方法没有传递回调函数";
+		}
+
+		// 创建一个canvas
+		const canvasEl = document.createElement('canvas')
+		canvasEl.width = this.videoEl.videoWidth
+		canvasEl.height = this.videoEl.videoHeight
+		// 截图
+		const ctx = canvasEl.getContext('2d')
+		ctx.drawImage(this.videoEl, 0, 0)
+
+		// 保存为base64
+		if(type === 'base64'){
+			const image = canvasEl.toDataURL('image/jpg')
+			return callback(image)
+		}
+		
+		//保存为png文件
+		if(type === 'file'){
+			// 处理兼容问题
+			if (!HTMLCanvasElement.prototype.toBlob) {
+				Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+					value: function(callback, type, quality) {
+						const binStr = atob(this.toDataURL(type, quality).split(',')[1])
+						const len = binStr.length
+						const arr = new Uint8Array(len)
+					
+						for (let i = 0; i < len; i++) {
+							arr[i] = binStr.charCodeAt(i)
+						}
+					
+						callback(new Blob([arr], { type: type || 'image/png' }))
+					}
+				})
+			}
+			
+			canvasEl.toBlob((blob) => {
+				let pngFile = new File(blob,fileName,'image/png')
+				callback(pngFile);
+			})
+			
+		}
+
+		
 	}
 
 	//开始录制
@@ -33,5 +91,3 @@ class tape {
 		});
 	}
 }
-
-export default tape
